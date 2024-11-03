ddf6a77603e0c9a65e9b5dd0ad92714233bad97a
Anna (Anya) Parker <50943381+anna-parker@users.noreply.github.com>
2024-11-03 20:14:12 +0100

    Add lineages to auspice.json

--- scripts/fix_auspice_tree.py
+++ scripts/fix_auspice_tree.py
@@ -13,6 +13,20 @@ logging.basicConfig(
     datefmt="%H:%M:%S",
 )
 
+def add_lineage(node):
+    if "children" in node:
+        for child in node["children"]:
+            add_lineage(child)
+    full_clade_name = node["node_attrs"]["clade_membership"]["value"]
+    lineage = None
+    if full_clade_name.startswith("MARV"):
+        lineage = "MARV"
+    if full_clade_name.startswith("RAVV"):
+        lineage = "RAVV"
+    if lineage:
+        node["node_attrs"]["lineage"] = {}
+        node["node_attrs"]["lineage"]["value"] = lineage
+
 
 @click.command(help="Drops the outgroup from the auspice-tree")
 @click.option("--auspice-tree", required=True, type=click.Path(exists=True))
@@ -35,19 +49,28 @@ def main(
         data = json.load(file)
 
     children = data['tree']['children']
-    # delete the outgroup
     for i, child in enumerate(children):
         if child["name"] == outgroup_name:
-            #del children[i]
+            #del children[i] # Keep the outgroup for now as this is the reconstructed root
             child['name'] = "Reconstructed Root using KX371887.3: Dianlovirus menglaense as Outgroup"
             break
-
     #data['tree']['name'] = "Reconstructed Root using KX371887.3: Dianlovirus menglaense as Outgroup"
+
+    print("Renamed outgroup successfully.")
+
+    add_lineage(data['tree'])
+
+    print("Added lineage information to nodes successfully.")
+
+    data["meta"]["colorings"] = data["meta"]["colorings"] + [{
+                "key": "lineage",
+                "title": "Lineage",
+                "type": "categorical"
+            }]
+
 
     with open(output_auspice_tree, 'w') as file:
         json.dump(data, file, indent=4)
-
-    print("Renamed root successfully.")
 
 
 
